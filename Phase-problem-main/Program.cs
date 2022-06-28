76de72a8146a4fb695e3590f3c1ba24d8833c024
Vladislav <vpinchukov@list.ru>
2022-06-28 08:42:18 +0300

    Добавлена новая форма для коэфф. Цернике

--- Phase-problem-main/Program.cs
+++ Phase-problem-main/Program.cs
@@ -16,7 +16,7 @@ namespace Phase_problem_main
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
